2793d677ab9ce4b36f24db638fbe1c76cb4e81b3
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2022-07-02 13:19:20 +0700

    create user

--- src/hooks/users/create-user.js
+++ src/hooks/users/create-user.js
@@ -2,10 +2,13 @@ const errors = require("@feathersjs/errors");
 
 module.exports = () => {
   return async (context) => {
-    const { app, result } = context;
+    const { app, result, data } = context;
     const sequelize = app.get("sequelizeClient");
     const { guru, siswa } = sequelize.models;
-    const { id_user, name, role } = result;
+    // result
+    const { id_user, role } = result;
+    // data
+    const { agama, photo, name } = data;
     // create guru
     if (role === "guru") {
       await guru.create({
@@ -18,6 +21,8 @@ module.exports = () => {
       await siswa.create({
         id_siswa: id_user,
         nama_siswa: name,
+        photo: photo,
+        agama: agama,
       });
     }
     return context;
