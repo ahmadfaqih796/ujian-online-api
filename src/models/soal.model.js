0b695cc1f51b20ee513c2b10b47a371fcda096a7
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-03-26 11:19:20 +0700

    soal model

--- src/models/soal.model.js
+++ src/models/soal.model.js
@@ -1,22 +1,69 @@
 // See https://sequelize.org/master/manual/model-basics.html
 // for more of what you can do here.
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 const DataTypes = Sequelize.DataTypes;
 
 module.exports = function (app) {
-  const sequelizeClient = app.get('sequelizeClient');
-  const soal = sequelizeClient.define('soal', {
-    text: {
-      type: DataTypes.STRING,
-      allowNull: false
+  const sequelizeClient = app.get("sequelizeClient");
+  const soal = sequelizeClient.define(
+    "soal",
+    {
+      id_soal: {
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+
+      kode_pelajaran: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      kode_kelas: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      semester: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      pertanyaan: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      pilihan_a: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      pilihan_b: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      pilihan_c: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      pilihan_d: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      pilihan_e: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      kunci: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      hooks: {
+        beforeCount(options) {
+          options.raw = true;
+        },
+      },
     }
-  }, {
-    hooks: {
-      beforeCount(options) {
-        options.raw = true;
-      }
-    }
-  });
+  );
 
   // eslint-disable-next-line no-unused-vars
   soal.associate = function (models) {
