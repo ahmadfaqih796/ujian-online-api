732a5df1ca331a943e956f562f0af6db7b0bfca0
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-03-28 11:19:20 +0700

    pelajaran model

--- src/models/pelajaran.model.js
+++ src/models/pelajaran.model.js
@@ -1,22 +1,32 @@
 // See https://sequelize.org/master/manual/model-basics.html
 // for more of what you can do here.
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 const DataTypes = Sequelize.DataTypes;
 
 module.exports = function (app) {
-  const sequelizeClient = app.get('sequelizeClient');
-  const pelajaran = sequelizeClient.define('pelajaran', {
-    text: {
-      type: DataTypes.STRING,
-      allowNull: false
+  const sequelizeClient = app.get("sequelizeClient");
+  const pelajaran = sequelizeClient.define(
+    "pelajaran",
+    {
+      id_pelajaran: {
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      nama_pelajaran: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      hooks: {
+        beforeCount(options) {
+          options.raw = true;
+        },
+      },
     }
-  }, {
-    hooks: {
-      beforeCount(options) {
-        options.raw = true;
-      }
-    }
-  });
+  );
 
   // eslint-disable-next-line no-unused-vars
   pelajaran.associate = function (models) {
