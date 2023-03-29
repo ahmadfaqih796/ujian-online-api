56502f9f0d2f5ae4697ca4c1bd6c6a3a2f5047d6
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-03-29 10:11:24 +0700

    auth

--- src/authentication.js
+++ src/authentication.js
@@ -1,13 +1,29 @@
-const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
-const { LocalStrategy } = require('@feathersjs/authentication-local');
-const { expressOauth } = require('@feathersjs/authentication-oauth');
+const {
+  AuthenticationService,
+  JWTStrategy,
+} = require("@feathersjs/authentication");
+const { LocalStrategy } = require("@feathersjs/authentication-local");
+const { expressOauth } = require("@feathersjs/authentication-oauth");
+const authResponse = require("../src/hooks/auth/auth-response");
+const invalidAuth = require("../src/hooks/auth/invalid-auth");
 
-module.exports = app => {
+module.exports = (app) => {
   const authentication = new AuthenticationService(app);
 
-  authentication.register('jwt', new JWTStrategy());
-  authentication.register('local', new LocalStrategy());
+  authentication.register("jwt", new JWTStrategy());
+  authentication.register("local", new LocalStrategy());
 
-  app.use('/authentication', authentication);
+  app.use("/authentication", authentication);
   app.configure(expressOauth());
+
+  const service = app.service("authentication");
+
+  service.hooks({
+    after: {
+      create: [authResponse()],
+    },
+    error: {
+      create: [invalidAuth()],
+    },
+  });
 };
