927ae9a5aa0b824ca748e0f124e9933d26b4189f
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-03-29 09:35:23 +0700

    invalid auth

---
+++ src/hooks/auth/invalid-auth.js
@@ -0,0 +1,25 @@
+const errors = require("@feathersjs/errors");
+
+// eslint-disable-next-line no-unused-vars
+module.exports = (options = {}) => {
+  return async (context) => {
+    const { app, data } = context;
+
+    if (context.error.message === "Invalid login") {
+      const email = data.email;
+      const isEmailValid = await app.service("users").Model.findOne({
+        where: {
+          email: email,
+        },
+      });
+
+      if (isEmailValid) {
+        throw new errors.NotAuthenticated("Email atau password salah");
+      }
+
+      throw new errors.NotAuthenticated("Akun tidak ditemukan");
+    }
+
+    return context;
+  };
+};
