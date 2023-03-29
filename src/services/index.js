1b449fd2fcd9e571d027bf8da0a67b36caabaaa8
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-03-29 09:22:01 +0700

    service pelajaran

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const users = require('./users/users.service.js');
 const soal = require('./soal/soal.service.js');
 const kelas = require('./kelas/kelas.service.js');
 const siswa = require('./siswa/siswa.service.js');
+const pelajaran = require('./pelajaran/pelajaran.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(soal);
   app.configure(kelas);
   app.configure(siswa);
+  app.configure(pelajaran);
 };
