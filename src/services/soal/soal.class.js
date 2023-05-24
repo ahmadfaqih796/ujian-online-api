45d0fab899d989cd06293be44ff31033a6067c8d
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-05-24 10:02:45 +0700

    soal

--- src/services/soal/soal.class.js
+++ src/services/soal/soal.class.js
@@ -1,5 +1,15 @@
-const { Service } = require('feathers-sequelize');
+const { Service } = require("feathers-sequelize");
+
+const errors = require("@feathersjs/errors");
 
 exports.Soal = class Soal extends Service {
-  
+  //   async create(data, params) {
+  //     try {
+  //       console.log("xxxxxxxxx", data);
+  //       return;
+  //     } catch (error) {
+  //       console.log(error);
+  //       throw new errors.GeneralError(error.message);
+  //     }
+  //   }
 };
