376b4784084cb4654879990d2eb796bde2cbc6a8
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-05-24 10:15:17 +0700

    library read-excel-file

--- src/services/soal/soal.class.js
+++ src/services/soal/soal.class.js
@@ -1,15 +1,16 @@
 const { Service } = require("feathers-sequelize");
+const readXlsxFile = require("read-excel-file/node");
 
 const errors = require("@feathersjs/errors");
 
 exports.Soal = class Soal extends Service {
-  //   async create(data, params) {
-  //     try {
-  //       console.log("xxxxxxxxx", data);
-  //       return;
-  //     } catch (error) {
-  //       console.log(error);
-  //       throw new errors.GeneralError(error.message);
-  //     }
-  //   }
+  async create(data, params) {
+    try {
+      console.log("masuk pak eko", data);
+      return data;
+    } catch (error) {
+      console.log(error);
+      throw new errors.GeneralError(error.message);
+    }
+  }
 };
