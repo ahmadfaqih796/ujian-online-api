768646d9f63ca04785e1b6263d9cc28ce486a692
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-05-26 14:11:04 +0700

    soal

--- src/services/soal/soal.service.js
+++ src/services/soal/soal.service.js
@@ -1,19 +1,20 @@
 // Initializes the `soal` service on path `/soal`
-const { Soal } = require('./soal.class');
-const createModel = require('../../models/soal.model');
-const hooks = require('./soal.hooks');
+const { Soal } = require("./soal.class");
+const createModel = require("../../models/soal.model");
+const hooks = require("./soal.hooks");
 
 module.exports = function (app) {
   const options = {
     Model: createModel(app),
-    paginate: app.get('paginate')
+    paginate: app.get("paginate"),
+    multi: ["create", "patch", "remove"],
   };
 
   // Initialize our service with any options it requires
-  app.use('/soal', new Soal(options, app));
+  app.use("/soal", new Soal(options, app));
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('soal');
+  const service = app.service("soal");
 
   service.hooks(hooks);
 };
