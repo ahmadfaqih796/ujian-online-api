14d33f4a7ce62ac1ada5007ec3733ac45781b07e
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-05-29 09:43:15 +0700

    hook admin include

--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -5,12 +5,23 @@ const { hashPassword, protect } =
 const createUser = require("../../hooks/users/create-user");
 const includeUserSiswa = require("../../hooks/users/include-user-siswa");
 const includeUserGuru = require("../../hooks/users/include-user-guru");
+const includeUserAdmin = require("../../hooks/users/include-user-admin");
 
 module.exports = {
   before: {
     all: [],
-    find: [authenticate("jwt"), includeUserSiswa(), includeUserGuru()],
-    get: [authenticate("jwt"), includeUserSiswa(), includeUserGuru()],
+    find: [
+      authenticate("jwt"),
+      includeUserSiswa(),
+      includeUserGuru(),
+      includeUserAdmin(),
+    ],
+    get: [
+      authenticate("jwt"),
+      includeUserSiswa(),
+      includeUserGuru(),
+      includeUserAdmin(),
+    ],
     create: [hashPassword("password")],
     update: [hashPassword("password"), authenticate("jwt")],
     patch: [hashPassword("password"), authenticate("jwt")],
