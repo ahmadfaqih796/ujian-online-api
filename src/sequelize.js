45e739d3140cd43b624dcc44cdd711bc99b7a6bc
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-06-05 21:47:52 +0700

    custom sequalize #1

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,31 +1,44 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
+
+const { DB_SYNC } = process.env;
 
 module.exports = function (app) {
-  const connectionString = app.get('mysql');
+  const connectionString = app.get("mysql");
   const sequelize = new Sequelize(connectionString, {
-    dialect: 'mysql',
+    dialect: "mysql",
     logging: false,
     define: {
-      freezeTableName: true
-    }
+      freezeTableName: true,
+    },
   });
   const oldSetup = app.setup;
 
-  app.set('sequelizeClient', sequelize);
+  app.set("sequelizeClient", sequelize);
 
   app.setup = function (...args) {
     const result = oldSetup.apply(this, args);
 
     // Set up data relationships
     const models = sequelize.models;
-    Object.keys(models).forEach(name => {
-      if ('associate' in models[name]) {
+    Object.keys(models).forEach((name) => {
+      if ("associate" in models[name]) {
         models[name].associate(models);
       }
     });
 
     // Sync to the database
-    app.set('sequelizeSync', sequelize.sync());
+    console.log("first", DB_SYNC);
+    if (DB_SYNC === "true") {
+      try {
+        app.set(
+          "sequelizeSync",
+          sequelize.sync({ alter: true, logging: console.log })
+        );
+        console.log("done");
+      } catch (error) {
+        console.log(error);
+      }
+    }
 
     return result;
   };
