2d4ecd54adf91b2c7f864d1ced92bbe9d581a2d4
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-06-06 08:39:52 +0700

    pelajaran hooks #1

--- src/services/pelajaran/pelajaran.hooks.js
+++ src/services/pelajaran/pelajaran.hooks.js
@@ -1,14 +1,43 @@
-const { authenticate } = require('@feathersjs/authentication').hooks;
+const { authenticate } = require("@feathersjs/authentication").hooks;
+
+const handleChildren = () => {
+  return async (context) => {
+    const { app, data, result } = context;
+    const sequelize = app.get("sequelizeClient");
+    const { soal } = sequelize.models;
+    if (data && data.children) {
+      const sidebarItems = data.children.map((item) => ({
+        ...item,
+        parent_id: result.id,
+      }));
+      await soal.bulkCreate(sidebarItems);
+    }
+    return context;
+  };
+};
+
+const handleDeleteChildren = () => {
+  return async (context) => {
+    const { app, id } = context;
+    const sequelize = app.get("sequelizeClient");
+    const { soal } = sequelize.models;
+    const itemCount = await soal.count({ where: { id_pelajaran: id } });
+    if (itemCount > 0) {
+      await soal.destroy({ where: { id_pelajaran: id } });
+    }
+    return context;
+  };
+};
 
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
+    all: [authenticate("jwt")],
     find: [],
     get: [],
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [],
   },
 
   after: {
@@ -18,7 +47,7 @@ module.exports = {
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [],
   },
 
   error: {
@@ -28,6 +57,6 @@ module.exports = {
     create: [],
     update: [],
     patch: [],
-    remove: []
-  }
+    remove: [],
+  },
 };
