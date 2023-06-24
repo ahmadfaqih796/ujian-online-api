2ce90554d66f26fda810c36608e4b384cf35a8a2
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-06-24 20:28:12 +0700

    fix export excel

--- src/services/export/users/users.class.js
+++ src/services/export/users/users.class.js
@@ -19,93 +19,16 @@ exports.Users = class Users {
     for (let i = 0; i < data.length; i++) {
       const element = {
         no: i + 1,
-        name: data[i].user_guru?.nama_guru ?? "-",
-        nip: data[i]?.user_guru?.nip ?? "-",
-        role: data[i]?.role ?? "-",
-        email: data[i]?.email ?? "-",
-        photo: data[i]?.user_guru?.photo ?? "-",
+        name: data[i].nama_guru ?? "-",
+        nip: data[i]?.nip ?? "-",
+        role: data[i]?.user_data?.role ?? "-",
+        email: data[i]?.user_data?.email ?? "-",
+        photo: data[i]?.photo ?? "-",
       };
       newData.push(element);
     }
 
     return newData;
-  }
-
-  async find(params) {
-    const sequelize = this.app.get("sequelizeClient");
-    const { users, guru } = sequelize.models;
-    const { name, role } = params.query;
-
-    // if (!company_id)
-    //   throw new errors.BadRequest("Params company_id must be fullfield");
-
-    const getUsers = await users.findAll({
-      attributes: ["role", "email", "createdAt"],
-      where: {
-        ...(name && {
-          name: {
-            $like: `%${name}%`,
-          },
-        }),
-        ...(role && {
-          role: role,
-        }),
-      },
-      // order: [["nama_guru", "ASC"]],
-      include: [
-        {
-          model: guru,
-          as: "user_guru",
-          attributes: ["nama_guru", "nip", "photo"],
-        },
-      ],
-      nested: true,
-      // raw: true,
-    });
-
-    const result = this.normalizeData(getUsers);
-
-    const columns = [
-      {
-        header: "No",
-        key: "no",
-        width: 5,
-      },
-      {
-        header: "name",
-        key: "name",
-        width: 30,
-      },
-      {
-        header: "NIP",
-        key: "nip",
-        width: 15,
-      },
-      {
-        header: "role",
-        key: "role",
-        width: 10,
-      },
-      {
-        header: "photo",
-        key: "photo",
-        width: 30,
-      },
-    ];
-
-    const workbook = new ExelJs.Workbook();
-    workbook.creator = "AHMAD FAQIH ARIFIN";
-    workbook.title = "Laporan";
-    // workbook.lastModifiedBy = "Faqih";
-    workbook.created = new Date();
-    workbook.modified = new Date();
-    // workbook.lastPrinted = new Date(2016, 9, 27);
-    const worksheet = workbook.addWorksheet("users");
-    worksheet.columns = columns;
-
-    worksheet.addRows(result);
-
-    return { workbook };
   }
 
   async create(data, params) {
@@ -114,23 +37,18 @@ exports.Users = class Users {
       const { users, guru } = sequelize.models;
       const { role } = data;
 
-      const getUsers = await users.findAll({
-        attributes: ["role", "email", "createdAt"],
-        where: {
-          ...(role && {
-            role: role,
-          }),
-        },
-        // order: [["nama_guru", "ASC"]],
+      const getUsers = await guru.findAll({
+        attributes: ["id_guru", "nama_guru", "nip", "photo"],
+        order: [["nama_guru", "ASC"]],
         include: [
           {
-            model: guru,
-            as: "user_guru",
-            attributes: ["nama_guru", "nip", "photo"],
+            model: users,
+            as: "user_data",
+            attributes: ["role", "email", "createdAt"],
           },
         ],
         nested: true,
-        // raw: true,
+        raw: true,
       });
 
       const result = this.normalizeData(getUsers);
