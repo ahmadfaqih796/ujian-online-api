07c518eaa65dccc19d2951080284f72ec340f2ce
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-06-27 23:52:35 +0700

    find total user

--- src/services/dashboard/dashboard.class.js
+++ src/services/dashboard/dashboard.class.js
@@ -1,3 +1,6 @@
+const { readSQLFile } = require("../../utils/query-helper");
+const { QueryTypes } = require("sequelize");
+
 /* eslint-disable no-unused-vars */
 exports.Dashboard = class Dashboard {
   constructor(options) {
@@ -7,7 +10,17 @@ exports.Dashboard = class Dashboard {
     this.app = app;
   }
   async find(params) {
-    return [];
+    const sequelize = this.app.get("sequelizeClient");
+    const SQL_FILE = readSQLFile("count-user-by-role");
+    const usersData = await sequelize.query(SQL_FILE, {
+      type: QueryTypes.SELECT,
+    });
+
+    const result = {
+      users_data: usersData,
+    };
+
+    return result;
   }
 
   async get(id, params) {
