1ba74e0a16fab43c320e8af558ac702efe4cfb5f
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-08-10 10:28:56 +0700

    delete soal

--- src/services/soal/soal.hooks.js
+++ src/services/soal/soal.hooks.js
@@ -1,15 +1,43 @@
 const { authenticate } = require("@feathersjs/authentication").hooks;
 const { disablePagination } = require("feathers-hooks-common");
 
+const handleDeleteMultiple = () => {
+  return async (context) => {
+    const { app, params } = context;
+    const sequelize = app.get("sequelizeClient");
+    const { soal } = sequelize.models;
+    const { id_soal } = params.query;
+
+    if (!params.query || !id_soal) {
+      throw new Error("data soal tidak boleh kosong");
+    }
+
+    const idSoal = id_soal.split(",");
+    try {
+      for (const id of idSoal) {
+        await soal.destroy({
+          where: { id_soal: id },
+        });
+      }
+      context.result = { message: "Delete Successful", id_soal: idSoal };
+    } catch (error) {
+      console.log("errrrrr", error);
+      throw error;
+    }
+
+    return context;
+  };
+};
+
 module.exports = {
   before: {
-    all: [],
+    all: [authenticate("jwt")],
     find: [disablePagination()],
     get: [disablePagination()],
     create: [],
     update: [],
     patch: [],
-    remove: [],
+    remove: [handleDeleteMultiple()],
   },
 
   after: {
