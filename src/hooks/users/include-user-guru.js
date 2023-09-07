0cb8195fdda2519d48817447701faf483781ee54
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-09-07 09:42:22 +0700

    search name guru

--- src/hooks/users/include-user-guru.js
+++ src/hooks/users/include-user-guru.js
@@ -5,15 +5,25 @@ const concatValue = (k, l, r) => (k === "include" ? concat(l, r) : r);
 // eslint-disable-next-line no-unused-vars
 module.exports = function (options = {}) {
   return async (context) => {
-    const sequelize = context.app.get("sequelizeClient");
+    const { app, params } = context;
+    const sequelize = app.get("sequelizeClient");
     const { guru } = sequelize.models;
+    const { query } = params;
     const include = [
       {
         attributes: { exclude: ["id_guru", "createdAt", "updatedAt"] },
         model: guru,
         as: "user_guru",
+        ...(query.name_user && {
+          where: {
+            nama_guru: {
+              $like: `%${query.name_user}%`,
+            },
+          },
+        }),
       },
     ];
+    if (query.name_user) delete context.params.query.name_user;
 
     const sequelizeOptions = mergeWithKey(
       concatValue,
