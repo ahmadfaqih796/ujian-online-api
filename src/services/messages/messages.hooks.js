30b36a0417d3e377b2ec7a4d44708975e7717e6d
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-09-15 08:55:08 +0700

    handle read user

--- src/services/messages/messages.hooks.js
+++ src/services/messages/messages.hooks.js
@@ -2,6 +2,35 @@ const { authenticate } = require("@feathersjs/authentication").hooks;
 const { disablePagination } = require("feathers-hooks-common");
 const includeUser = require("../../hooks/includes/include-user");
 
+const handleReadUser = () => {
+  return async (context) => {
+    const { app, params } = context;
+    const sequelize = app.get("sequelizeClient");
+    const { messages } = sequelize.models;
+    const { id_sender } = params.query;
+    try {
+      const res = await messages.update(
+        {
+          is_read: 1,
+        },
+        {
+          where: {
+            id_sender: id_sender,
+          },
+        }
+      );
+      context.result = {
+        message: "Berhasil Read Data",
+        total_read: res,
+      };
+    } catch (error) {
+      console.log("error", error);
+      throw new Error("error data");
+    }
+    return context;
+  };
+};
+
 module.exports = {
   before: {
     all: [
@@ -11,7 +40,7 @@ module.exports = {
     get: [disablePagination(), includeUser()],
     create: [],
     update: [],
-    patch: [],
+    patch: [handleReadUser()],
     remove: [],
   },
 
